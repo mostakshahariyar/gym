d902c7ea21abc8071dfc6fd5943d9b1ddc9b6fa4
mostakshahariyar <mostakshahariyar18@gmail.com>
2023-07-30 04:06:44 +0600

    add btn in details section

--- src/component/Details/Details.js
+++ src/component/Details/Details.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Details = () => {
         return (
-                <div>
-                        <h1 className='text-center h-screen flex justify-center items-center bg-gray-300 text-5xl font-mono'>We are working this section...</h1>
+                <div className='h-screen flex flex-col gap-5 justify-center items-center bg-gray-300'>
+                        <h1 className='text-center   text-5xl font-mono'>We are working this section...</h1>
+                        
+                        <Link to="/team">
+                        <button className='py-2 px-4 bg-gray-500 text-white font-semibold rounded-lg shadow-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75 text-xl'>Back to previews page</button>
+                        </Link>
                 </div>
         );
 };
