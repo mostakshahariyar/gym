8d6657621ae1d256b1719039f5c41ec38509f5af
mostakshahariyar <mostakshahariyar18@gmail.com>
2023-07-29 16:58:46 +0600

    responsive team

--- src/component/Team/Team.js
+++ src/component/Team/Team.js
@@ -4,7 +4,7 @@ const Team = () => {
         return (
                 <div className='bg-gray-300 w-100'>
                         <p className='text-4xl text-center font-extrabold uppercase py-5 font-sans'>Meet The pros</p>
-                        <p className='text-center w-[50%] font-mono text-2xl mx-auto pb-5'>A gym team typically refers to a group of individuals who come together to participate in fitness activities and workouts at a gym or fitness center.</p>
+                        <p className='lg:text-center text-justify lg:p-[0] px-[.5rem] w-[100%] lg:w-[50%] font-mono text-2xl mx-auto pb-5'>A gym team typically refers to a group of individuals who come together to participate in fitness activities and workouts at a gym or fitness center.</p>
                 </div>
         );
 };
