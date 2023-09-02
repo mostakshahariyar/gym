68397d04b45a51934dd2696b311c091723e22e82
mostakshahariyar <mostakshahariyar18@gmail.com>
2023-09-02 21:46:13 +0600

    notfound page edit

--- src/component/NotFound/NotFound.js
+++ src/component/NotFound/NotFound.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NotFound = () => {
         return (
-                <div>
-                        <h1>Sorry we work this section. </h1>
+                <div className='flex justify-center items-center h-screen bg-gray-400'>
+                        <h1 className='text-4xl font-bold text-gray-800'>404 | page not found</h1>
                 </div>
         );
 };
